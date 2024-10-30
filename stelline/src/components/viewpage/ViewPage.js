a916f7e1d9910833343d874d301905347f958f94
gom125 <maplepro@naver.com>
2024-10-30 19:31:08 +0900

    Add test page

---
+++ stelline/src/components/viewpage/ViewPage.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './ViewPage.css';
+
+const ViewPage = () => {
+
+    return (
+        <div className="test">
+            <p>test</p>
+        </div>
+    );
+};
